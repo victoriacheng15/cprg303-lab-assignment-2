0ab0c53a317ff6fed2bf4606da5ddf53339fff6c
Victoria Cheng <victoria.cheng15@gmail.com>
2025-03-05 19:20:40 -0700

    add lab 6 for the stack screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,22 +1,23 @@
 import { Stack } from "expo-router/stack";
 
 export default function Layout() {
-	return (
-		<Stack
-			screenOptions={{
-				headerStyle: {
-					backgroundColor: "#0096FF",
-				},
-				headerTintColor: "#fff",
-				headerTitleStyle: {
-					fontWeight: "bold",
-				},
-			}}
-		>
-			<Stack.Screen name="index" options={{ title: "Home" }} />
-			<Stack.Screen name="lab3" options={{ title: "Lab 3" }} />
-			<Stack.Screen name="lab4" options={{ title: "Lab 4" }} />
-			<Stack.Screen name="lab5" options={{ title: "Lab 5" }} />
-		</Stack>
-	);
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#0096FF",
+        },
+        headerTintColor: "#fff",
+        headerTitleStyle: {
+          fontWeight: "bold",
+        },
+      }}
+    >
+      <Stack.Screen name="index" options={{ title: "Home" }} />
+      <Stack.Screen name="lab3" options={{ title: "Lab 3" }} />
+      <Stack.Screen name="lab4" options={{ title: "Lab 4" }} />
+      <Stack.Screen name="lab5" options={{ title: "Lab 5" }} />
+      <Stack.Screen name="lab6" options={{ title: "Lab 6" }} />
+    </Stack>
+  );
 }
